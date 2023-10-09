a8bd7b6be57e8a0700b5b0b5022af488ecfc0c99
byounghoonpark <88michael@naver.com>
2023-10-09 21:29:30 +0900

    feat: project build

---
+++ src/test/java/com/example/safedn/SafednApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safedn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafednApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
